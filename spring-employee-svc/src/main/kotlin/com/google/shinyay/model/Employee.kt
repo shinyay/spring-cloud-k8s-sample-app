427fc3183c577235dd9a713a65ed8f3d0d2e10bd
shinyay <shinya.com@gmail.com>
2020-05-13 23:21:29 +0900

    Add document annotation for Mongo DB

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/model/Employee.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/model/Employee.kt
@@ -1,7 +1,9 @@
 package com.google.shinyay.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document
 data class Employee (@Id val id: String,
                      val organizationId: Long,
                      val departmentId: Long,

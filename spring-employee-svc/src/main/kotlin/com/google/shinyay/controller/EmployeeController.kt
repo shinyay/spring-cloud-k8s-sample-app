0e9dd5ac09e25ea1270eab98e200d69dc92de472
shinyay <shinya.com@gmail.com>
2020-05-14 07:29:46 +0900

    Add employee add function with POST method

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,4 +1,17 @@
 package com.google.shinyay.controller
 
-class EmployeeController {
+import com.google.shinyay.logger
+import com.google.shinyay.model.Employee
+import com.google.shinyay.repository.EmployeeRepository
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EmployeeController(val repository: EmployeeRepository) {
+
+    @PostMapping("/")
+    fun add(employee: Employee): Employee {
+        logger.info("Employee add: $employee")
+        return repository.save(employee)
+    }
 }

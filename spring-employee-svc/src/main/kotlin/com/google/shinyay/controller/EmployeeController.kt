ce020e8d0b59e037dfe62fd617a3fb242d082816
shinyay <shinya.com@gmail.com>
2020-05-14 07:30:39 +0900

    Add parameter configuration for RequestBody

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -4,13 +4,14 @@ import com.google.shinyay.logger
 import com.google.shinyay.model.Employee
 import com.google.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class EmployeeController(val repository: EmployeeRepository) {
 
     @PostMapping("/")
-    fun add(employee: Employee): Employee {
+    fun add(@RequestBody employee: Employee): Employee {
         logger.info("Employee add: $employee")
         return repository.save(employee)
     }

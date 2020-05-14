6e76e21076ce61a95676c2aa1fef017ea6efbbed
shinyay <shinya.com@gmail.com>
2020-05-14 22:17:32 +0900

    Add find Employee by id function

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -3,9 +3,7 @@ package com.google.shinyay.controller
 import com.google.shinyay.logger
 import com.google.shinyay.model.Employee
 import com.google.shinyay.repository.EmployeeRepository
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class EmployeeController(val repository: EmployeeRepository) {
@@ -14,5 +12,11 @@ class EmployeeController(val repository: EmployeeRepository) {
     fun add(@RequestBody employee: Employee): Employee {
         logger.info("Employee add: $employee")
         return repository.save(employee)
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable("id") id: String): Employee {
+        logger.info("Employee find: id=$id")
+        return repository.findById(id).get()
     }
 }

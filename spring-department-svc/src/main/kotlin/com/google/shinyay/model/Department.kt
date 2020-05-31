11d8563c14ba11dbf5cb9aa09ab23243d6d20bc0
shinyay <shinya.com@gmail.com>
2020-06-01 07:45:46 +0900

    Add specification of the name for the collection where databasr will be stored

--- spring-department-svc/src/main/kotlin/com/google/shinyay/model/Department.kt
+++ spring-department-svc/src/main/kotlin/com/google/shinyay/model/Department.kt
@@ -1,6 +1,9 @@
 package com.google.shinyay.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "department")
 data class Department(@Id val id: String,
                       val organizationId: Long,
                       val name: String,

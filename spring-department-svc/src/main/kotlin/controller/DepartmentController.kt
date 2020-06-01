62eb47c6d8ebcb9096c12c2f8532060b35533831
shinyay <shinya.com@gmail.com>
2020-06-01 22:59:31 +0900

    Add feign client interface

--- spring-department-svc/src/main/kotlin/controller/DepartmentController.kt
+++ spring-department-svc/src/main/kotlin/controller/DepartmentController.kt
@@ -1,11 +1,15 @@
 package controller
 
 import com.google.shinyay.client.EmployeeClient
+import com.google.shinyay.model.Employee
 import com.google.shinyay.repository.DepartmentRepository
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DepartmentController(val repository: DepartmentRepository,
                            val employeeClient: EmployeeClient) {
 
+    @GetMapping("/feign")
+    fun listRest(): List<Employee> = employeeClient.findByDepartment("1")
 }

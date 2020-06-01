61752404a17b5b5bf6bfbc245011234837e7563d
shinyay <shinya.com@gmail.com>
2020-06-01 22:23:55 +0900

    Add findByDepartment function for Employee

--- spring-department-svc/src/main/kotlin/com/google/shinyay/client/EmployeeClient.kt
+++ spring-department-svc/src/main/kotlin/com/google/shinyay/client/EmployeeClient.kt
@@ -1,4 +1,12 @@
 package com.google.shinyay.client
 
-class EmployeeClient {
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
+@FeignClient(name = "employee")
+interface EmployeeClient {
+
+    @GetMapping("/department/{departmentId}")
+    fun findByDepartment(@PathVariable("departmentId") departmentId: String)
 }

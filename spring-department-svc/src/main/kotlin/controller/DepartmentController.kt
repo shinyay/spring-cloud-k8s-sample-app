8e2f0add03b6d78c14708e0bff1c252118ba1f40
shinyay <shinya.com@gmail.com>
2020-06-01 23:04:14 +0900

    Add department information save function with POST

--- spring-department-svc/src/main/kotlin/controller/DepartmentController.kt
+++ spring-department-svc/src/main/kotlin/controller/DepartmentController.kt
@@ -1,9 +1,13 @@
 package controller
 
 import com.google.shinyay.client.EmployeeClient
+import com.google.shinyay.logger
+import com.google.shinyay.model.Department
 import com.google.shinyay.model.Employee
 import com.google.shinyay.repository.DepartmentRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,4 +16,10 @@ class DepartmentController(val repository: DepartmentRepository,
 
     @GetMapping("/feign")
     fun listRest(): List<Employee> = employeeClient.findByDepartment("1")
+
+    @PostMapping("/")
+    fun add(@RequestBody department: Department): Department {
+        logger.info("Department add: $department")
+        return repository.save(department)
+    }
 }

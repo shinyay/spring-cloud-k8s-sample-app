ef53b21957f2fd9eee09a8dccff27809a3885802
shinyay <shinya.com@gmail.com>
2020-06-05 07:23:44 +0900

    Add Ribbon Configuration Annotation to enable Ribbon

--- spring-department-svc/src/main/kotlin/com/google/shinyay/SpringCloudKubernetesApplication.kt
+++ spring-department-svc/src/main/kotlin/com/google/shinyay/SpringCloudKubernetesApplication.kt
@@ -1,10 +1,13 @@
 package com.google.shinyay
 
+import com.google.shinyay.config.RibbonConfiguration
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.ribbon.RibbonClients
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
@@ -12,7 +15,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableDiscoveryClient
 @EnableMongoRepositories
 @EnableFeignClients
-
+@AutoConfigureAfter(RibbonConfiguration::class)
+@RibbonClients(defaultConfiguration = [RibbonConfiguration::class])
 class SpringCloudKubernetesApplication
 
 fun main(args: Array<String>) {

515a41dca1f699c429cf37051ab7f84d81195203
shinyay <shinya.com@gmail.com>
2020-06-06 22:01:39 +0900

    Remove curly brace

--- spring-organization-svc/src/main/kotlin/com/google/shinyay/repository/OrganizationRepository.kt
+++ spring-organization-svc/src/main/kotlin/com/google/shinyay/repository/OrganizationRepository.kt
@@ -3,5 +3,4 @@ package com.google.shinyay.repository
 import com.google.shinyay.model.Organization
 import org.springframework.data.repository.CrudRepository
 
-interface OrganizationRepository : CrudRepository<Organization, String> {
-}
+interface OrganizationRepository : CrudRepository<Organization, String>

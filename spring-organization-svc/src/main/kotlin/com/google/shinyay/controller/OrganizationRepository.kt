b908738f9a5fbf8270bbd8b0f06a1511f4158125
shinyay <shinya.com@gmail.com>
2020-06-07 08:49:18 +0900

    Add Get function to find organization by id

--- spring-organization-svc/src/main/kotlin/com/google/shinyay/controller/OrganizationRepository.kt
+++ spring-organization-svc/src/main/kotlin/com/google/shinyay/controller/OrganizationRepository.kt
@@ -3,6 +3,7 @@ package com.google.shinyay.controller
 import com.google.shinyay.logger
 import com.google.shinyay.model.Organization
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -13,4 +14,11 @@ class OrganizationRepository(val repository: OrganizationRepository) {
         logger.info("Organization find")
         return repository.findAll()
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable("id") id: String): Organization {
+        logger.info("Organization find: id=$id")
+        return repository.findById(id)
+    }
+
 }

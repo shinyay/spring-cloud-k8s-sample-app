0fdeeb733495d451cb02f205762a1e054dfc1e49
shinyay <shinya.com@gmail.com>
2020-06-06 21:53:49 +0900

    Modify id type as Long

--- spring-organization-svc/src/main/kotlin/com/google/shinyay/model/Employee.kt
+++ spring-organization-svc/src/main/kotlin/com/google/shinyay/model/Employee.kt
@@ -1,6 +1,6 @@
 package com.google.shinyay.model
 
-data class Employee(val id: String,
+data class Employee(val id: Long,
                     val name: String,
                     val age: Int,
                     val position: String)

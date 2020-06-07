2d6c5e5fb7480c9ebc960e39b97c3248f5b3e933
shinyay <shinya.com@gmail.com>
2020-06-07 20:51:04 +0900

    Add Ribbon configuration for Feign Client

--- spring-organization-svc/src/main/kotlin/com/google/shinyay/config/RibbonConfiguration.kt
+++ spring-organization-svc/src/main/kotlin/com/google/shinyay/config/RibbonConfiguration.kt
@@ -1,4 +1,22 @@
 package com.google.shinyay.config
 
-class RibbonConfiguration {
+import com.netflix.client.config.IClientConfig
+import com.netflix.loadbalancer.PingUrl
+import com.netflix.loadbalancer.WeightedResponseTimeRule
+import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.context.annotation.Bean
+
+class RibbonConfiguration(val discoveryClient: DiscoveryClient) {
+
+    constructor(discoveryClient: DiscoveryClient, serviceId: String) : this(discoveryClient) {
+        this.serviceId = serviceId
+    }
+
+    var serviceId = "client"
+
+    @Bean
+    fun ribbonPing(config: IClientConfig) = PingUrl(false, "/actuator/health")
+
+    @Bean
+    fun ribbonRule(config: IClientConfig) = WeightedResponseTimeRule()
 }

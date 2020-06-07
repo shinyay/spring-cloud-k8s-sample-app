07ca7651c780dba275f18acaa2068d148b74f95b
shinyay <shinya.com@gmail.com>
2020-06-07 09:38:44 +0900

    Add Get function to access Employee Service through Feign Client

--- spring-organization-svc/src/main/kotlin/com/google/shinyay/client/EmployeeClient.kt
+++ spring-organization-svc/src/main/kotlin/com/google/shinyay/client/EmployeeClient.kt
@@ -1,7 +1,13 @@
 package com.google.shinyay.client
 
+import com.google.shinyay.model.Employee
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @FeignClient("employee")
 interface EmployeeClient {
+
+    @GetMapping("/organization/{organizationId}")
+    fun findByOrganization(@PathVariable("organizationId") organizationId: String): List<Employee>
 }

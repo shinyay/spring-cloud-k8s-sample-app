c645587953e12954acfdae237a0b42028c9393ea
shinyay <shinya.com@gmail.com>
2020-06-07 09:23:21 +0900

    Add Get function to access Department Service through Feign Client

--- spring-organization-svc/src/main/kotlin/com/google/shinyay/client/DepartmentClient.kt
+++ spring-organization-svc/src/main/kotlin/com/google/shinyay/client/DepartmentClient.kt
@@ -1,7 +1,13 @@
 package com.google.shinyay.client
 
+import com.google.shinyay.model.Department
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @FeignClient(name = "department")
-class DepartmentClient {
+interface DepartmentClient {
+
+    @GetMapping("/organization/{organizationId}")
+    fun findByOrganization(@PathVariable("organizationId") organizationId: String): List<Department>
 }

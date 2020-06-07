c01ae706f99126ff64e2ffdfb3bdbbc7ceb36731
shinyay <shinya.com@gmail.com>
2020-06-07 21:38:27 +0900

    Add jib configuration for container repository

--- spring-organization-svc/build.gradle.kts
+++ spring-organization-svc/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.0.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("com.gorylenko.gradle-git-properties") version "2.0.0"
+	id("com.google.cloud.tools.jib") version "2.1.0"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 }
@@ -50,4 +52,34 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
 	}
+}
+
+val project_id = if (hasProperty("docker_project_id")) findProperty("docker_project_id") as String else "library"
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+//	builder = "cloudfoundry/cnb:bionic"
+	builder = "gcr.io/buildpacks/builder"
+	imageName = "docker.io/${project_id}/${project.name}:${project.version}"
+}
+
+val username= if (hasProperty("docker_username")) findProperty("docker_username") as String else ""
+val password = if (hasProperty("docker_password")) findProperty("docker_password") as String else ""
+
+jib {
+	from {
+		image = "openjdk:11-slim"
+	}
+	to {
+		image = "registry.hub.docker.com/${project_id}/${project.name}:${project.version}"
+		auth.username = username
+		auth.password = password
+	}
+	container {
+		jvmFlags = mutableListOf("-Xms512m", "-Xdebug")
+		creationTime = "USE_CURRENT_TIMESTAMP"
+	}
+}
+
+springBoot {
+	buildInfo()
 }

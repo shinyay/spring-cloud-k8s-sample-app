dec22396708b49eb9d2472ae0e4aff0302a12965
shinyay <shinya.com@gmail.com>
2020-06-09 07:47:53 +0900

    Add BuildProperties to detect build version in Spring Boot

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/config/SwaggerConfig.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/config/SwaggerConfig.kt
@@ -1,4 +1,10 @@
 package com.google.shinyay.config
 
-class SwaggerConfig {
+import org.springframework.boot.info.BuildProperties
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig(val build: BuildProperties) {
 }

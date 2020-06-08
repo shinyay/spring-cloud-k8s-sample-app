e21fe68b4a5460cb0a90c820ad548126f35ca77b
shinyay <shinya.com@gmail.com>
2020-06-08 22:23:39 +0900

    Add Zuul Proxy enabled

--- spring-gateway-svc/src/main/kotlin/com/google/shinyay/SpringCloudKubernetesApplication.kt
+++ spring-gateway-svc/src/main/kotlin/com/google/shinyay/SpringCloudKubernetesApplication.kt
@@ -2,8 +2,12 @@ package com.google.shinyay
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 class SpringCloudKubernetesApplication
 
 fun main(args: Array<String>) {

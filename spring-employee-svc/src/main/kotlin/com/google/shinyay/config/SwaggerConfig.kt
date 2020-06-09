a0b188d3a4051c7f8aecff5fbac5725afbe060da
shinyay <shinya.com@gmail.com>
2020-06-10 07:36:41 +0900

    Add Swagger Document Configuration

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/config/SwaggerConfig.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/config/SwaggerConfig.kt
@@ -2,11 +2,23 @@ package com.google.shinyay.config
 
 import org.springframework.boot.info.BuildProperties
 import org.springframework.boot.info.GitProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
 class SwaggerConfig(val build: BuildProperties, val git: GitProperties) {
 
+    @Bean
+    fun docApi(): Docket = Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(ApiInfoBuilder()
+                        .version("${build.version}-${git.shortCommitId}-${git.branch}")
+                        .title("Employee API")
+                        .description("Employee Service API - ${build.version}-${git.shortCommitId}-${git.branch}")
+                        .build()
+                )
 }

1ac989350bc4160fd1d03bf273a981f529a67770
shinyay <shinya.com@gmail.com>
2020-06-09 22:18:04 +0900

    Agg Git Info for Swagger Document

--- spring-employee-svc/src/main/kotlin/com/google/shinyay/config/SwaggerConfig.kt
+++ spring-employee-svc/src/main/kotlin/com/google/shinyay/config/SwaggerConfig.kt
@@ -1,10 +1,12 @@
 package com.google.shinyay.config
 
 import org.springframework.boot.info.BuildProperties
+import org.springframework.boot.info.GitProperties
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
-class SwaggerConfig(val build: BuildProperties) {
+class SwaggerConfig(val build: BuildProperties, val git: GitProperties) {
+
 }

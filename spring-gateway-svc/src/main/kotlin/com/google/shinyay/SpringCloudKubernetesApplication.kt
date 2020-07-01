982ac5a6994c6dd088c5480efce0d2590c77cff1
shinyay <shinya.com@gmail.com>
2020-07-02 07:56:23 +0900

    Add Extension property as Logging function

--- spring-gateway-svc/src/main/kotlin/com/google/shinyay/SpringCloudKubernetesApplication.kt
+++ spring-gateway-svc/src/main/kotlin/com/google/shinyay/SpringCloudKubernetesApplication.kt
@@ -1,5 +1,7 @@
 package com.google.shinyay
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
@@ -13,3 +15,6 @@ class SpringCloudKubernetesApplication
 fun main(args: Array<String>) {
 	runApplication<SpringCloudKubernetesApplication>(*args)
 }
+
+val Any.logger: Logger
+	get() = LoggerFactory.getLogger(this.javaClass)
